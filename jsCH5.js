8a42eb3f434a0d8113811645f0b33729c9974951
Nadilaaaa12 <nadilazaskiaauliaaulia@gmail.com>
2024-04-09 11:15:35 +0700

    First

---
+++ jsCH5.js
@@ -0,0 +1,11 @@
+video.addEventListener("click",function video(){
+    alert("hallo ColdHeads୭ ˚.");
+});
+
+audio.addEventListener("click",function audio(){
+    alert("hallo  ColdHeads ୨ৎ selamat mendengarkan");
+});
+
+gambar.addEventListener("click",function gambar(){
+    alert("hallo ColdHeads :)");
+});
